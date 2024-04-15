8f95823acdfe5dbe88a3f7cbfc2fefab035d1ee0
Glenn Perez <149605766+gperez314@users.noreply.github.com>
2024-04-15 01:20:34 -0600

    Lock console on AI turn

--- Program.cs
+++ Program.cs
@@ -35,12 +35,14 @@ class AI : Player
     public override async Task<int> Turn()
     {
         Display.PlayerTurn(Name, ID);
+        // Console is locked
         Console.CancelKeyPress += (_, e) => e.Cancel = true;
-        await Task.Delay(1000);
+        await Task.Delay(1500);
         int playerTurn = (new Random()).Next(0, 7);
         Console.Write(playerTurn + 1);
-        await Task.Delay(1000);
+        await Task.Delay(1500);
         Console.WriteLine("");
+        // Console is locked
         Console.CancelKeyPress += (_, e) => e.Cancel = false;
         return playerTurn;
     }
