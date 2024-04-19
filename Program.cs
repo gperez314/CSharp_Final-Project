4cf4160b60fe133fa472d70a59cc848f132f3050
g.perez314 <g.perez314@mybvc.ca>
2024-04-19 15:16:09 -0600

    bug fixes

--- Program.cs
+++ Program.cs
@@ -1,80 +1,102 @@
-﻿// Updated Board
-
-using System.Data.Common;
-using System.Diagnostics;
-using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Xml.Linq;
+﻿
+// ==============================================================================================================
+// Player Class: Class to represent the players who will play the game
+// ==============================================================================================================
+using static System.Net.Mime.MediaTypeNames;
 
 class Player
 {
+    // Define properties for Player Name and ID
     public string Name { get; set; }
     public int ID { get; set; }
 
-
+    // Construtor for the Player class
     public Player(string name, int id)
     {
         Name = name;
         ID = id;
     }
+
+    // Override ToString method to display the player name
     public override string ToString()
     {
         return $"Player: {Name}";
-    }   
+    }
 
+    // Method for the player to make a turn
     public virtual async Task<int> Turn()
     {
+        // Display player's turn message
         Display.PlayerTurn(Name, ID);
         int playerTurn;
+        // Get player turn input from console and check for errors
         try
         {
+            // Read data from console
             playerTurn = int.Parse(Console.ReadLine()) - 1;
+            // Check if valid range
             if (playerTurn < 0 || playerTurn > 6)
                 throw new ArgumentOutOfRangeException();
         }
-        catch (ArgumentOutOfRangeException) 
+        // Error handler for input outside of valid column range
+        catch (ArgumentOutOfRangeException)
         {
             Console.WriteLine("Invalid move. Column no. out of range!");
-            playerTurn = Turn().Result;
+            playerTurn = Turn().Result; // Repeat player turn
         }
+        // Error handler for invalid format inputs
         catch
         {
             Console.WriteLine("Invalid move. Not a valid column no.!");
-            playerTurn = Turn().Result;
+            playerTurn = Turn().Result; // Repeat player turn
         }
-        return playerTurn;
+        return playerTurn; // Return column of player's turn
     }
 }
 
+
+// ==============================================================================================================
+// AI Class: Derived from a player class. An AI is a player who has capability to make a move on its own
+// ==============================================================================================================
 class AI : Player
 {
-    public AI(string name, int id) : base(name, id) {}
+    // Construtor for the AI class calling the base constructor of Player class
+    public AI(string name, int id) : base(name, id) { }
+
+    // Method for the AI to make a turn. The method is overriden to give the AI
+    // the ability to make its own move.
     public override async Task<int> Turn()
     {
+        // Display AI's turn message
         Display.PlayerTurn(Name, ID);
+        // Disable console inputs and add a delay
         Console.CancelKeyPress += (_, e) => e.Cancel = true;
         await Task.Delay(500);
+        // Select a random number from 0-7 to represent the turn and display on console
         int playerTurn = (new Random()).Next(0, 7);
         Console.Write(playerTurn + 1);
         await Task.Delay(500);
         Console.WriteLine("");
+        // Re-enable console inputs
         Console.CancelKeyPress += (_, e) => e.Cancel = false;
-        return playerTurn;
+        return playerTurn; // Return column of AI's turn
     }
 }
 
 class Board
 {
+    // Define properties for BoardState and number of rows and columns
     public int[,] BoardState { get; set; }
+    public static int Rows { get; private set; } = 7; // private set
+    public static int Columns { get; private set; } = 7; // private set
 
-    public static int Rows { get; private set; } = 7;
-    public static int Columns { get; private set; } = 7;
-
+    // Construtor for the Board class
     public Board()
     {
+        // Create a 2D array to represent the board state
         BoardState = new int[Rows, Columns];
 
-        // Initialize each element to zero
+        // Initialize each board state element to zero
         for (int i = 0; i < Rows; i++)
         {
             for (int j = 0; j < Columns; j++)
@@ -84,110 +106,139 @@ class Board
         }
     }
 
+    // Method to update the boardstate based on the player's move
     public void UpdateBoard(Player player, int mark)
     {
+        // Get the next available cell in the selected column
         int nextCell = GetNextAvailCell(mark);
-
+        // Check if there is an available cell in the selected column
         if (nextCell >= 0)
         {
+            // Cell is available. Update board state.
             BoardState[nextCell, mark] = player.ID;
         }
         else
         {
+            // Display invalid move message and ask the player to select a different turn
             Console.WriteLine("Invalid move. Column is already full!");
-            Connect4Game.MakeTurn(player);
+            Connect4Game.MakeTurn(player); // Repeat player's turn function
         }
     }
 
+    // Method to get the next available cell in the selected column
     private int GetNextAvailCell(int column)
     {
         for (int i = Rows - 1; i >= 0; i--)
         {
             if (BoardState[i, column] == 0)
-                return i;
+                return i; // Available cell found in the current column
         }
-        return -1;
+        return -1; // No more available cells
     }
 
+    // Method to determine if the board is already full
+    public bool IsBoardFull()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                if (BoardState[i, j] == 0)
+                    return false; // Board is not yet full
+            }
+        }
+        return true; // Board already full
+    }
 
+    // Method to check for the winning combinations in the current board
+    // A CheckWin algorithm written in Java was found and utilized
+    // Source: https://stackoverflow.com/questions/32770321/connect-4-check-for-a-win-algorithm
     public bool CheckWin(int player)
     {
-        // horizontalCheck 
+        // Check for horizontal combinations
         for (int j = 0; j < Rows - 3; j++)
         {
             for (int i = 0; i < Columns; i++)
             {
-                if (BoardState[i,j] == player && BoardState[i,j + 1] == player && 
-                    BoardState[i,j + 2] == player && BoardState[i,j + 3] == player)
+                if (BoardState[i, j] == player && BoardState[i, j + 1] == player &&
+                    BoardState[i, j + 2] == player && BoardState[i, j + 3] == player)
                 {
-                    return true;
+                    return true; // combination found
                 }
             }
         }
 
-        // verticalCheck
+        // Check for vertical combinations
         for (int i = 0; i < Columns - 3; i++)
         {
             for (int j = 0; j < Rows; j++)
             {
-                if (BoardState[i,j] == player && BoardState[i + 1,j] == player && 
-                    BoardState[i + 2,j] == player && BoardState[i + 3,j] == player)
+                if (BoardState[i, j] == player && BoardState[i + 1, j] == player &&
+                    BoardState[i + 2, j] == player && BoardState[i + 3, j] == player)
                 {
-                    return true;
+                    return true; // combination found
                 }
             }
         }
 
-        // ascendingDiagonalCheck 
+        // Check for ascending diagonal combinations
         for (int i = 3; i < Columns; i++)
         {
             for (int j = 0; j < Rows - 3; j++)
             {
-                if (BoardState[i,j] == player && BoardState[i - 1,j + 1] == player && 
-                    BoardState[i - 2,j + 2] == player && BoardState[i - 3,j + 3] == player)
-                    return true;
+                if (BoardState[i, j] == player && BoardState[i - 1, j + 1] == player &&
+                    BoardState[i - 2, j + 2] == player && BoardState[i - 3, j + 3] == player)
+                    return true; // combination found
             }
         }
 
-        // descendingDiagonalCheck
+        // Check for descending diagonal combinations
         for (int i = 3; i < Columns; i++)
         {
             for (int j = 3; j < Rows; j++)
             {
-                if (BoardState[i,j] == player && BoardState[i - 1,j - 1] == player && 
-                    BoardState[i - 2,j - 2] == player && BoardState[i - 3,j - 3] == player)
-                    return true;
+                if (BoardState[i, j] == player && BoardState[i - 1, j - 1] == player &&
+                    BoardState[i - 2, j - 2] == player && BoardState[i - 3, j - 3] == player)
+                    return true; // combination found
             }
         }
 
-        return false;
+        return false; // No winning combination found
     }
 }
 
 
+
 class Connect4Game
 {
-    private static int _turn { get; set; }
-    private static bool _win { get; set; }
-    private static int _mode { get; set; }
     private static Player _player1 { get; set; }
     private static Player _player2 { get; set; }
-
     private static Board _board { get; set; }
-
+    private static int _turn { get; set; } // current turn count
+    private static bool _win { get; set; } // win condition
+    private static bool _boardFull { get; set; } // board cells full
+    private static int _mode { get; set; } // game mode [0: 2-Player , 1: vs. Computer] 
 
     static Connect4Game()
     {
         _turn = 0;
         _win = false;
+        _boardFull = false;
+        _mode = 0;
     }
 
     public static void SetupGame()
     {
-        Display.GetGameMode();
-        _mode = GameMode();
-        Display.ShowGameMode(_mode);
+        _turn = 0;
+        _win = false;
+        _boardFull = false;
+        _mode = 0;
 
+        _board = new Board();
+
+        Display.GetGameMode();
+        _mode = GameMode(); 
+        Display.ShowGameMode(_mode);
         Display.GetPlayerName(" Please enter Player 1's name: ", 1);
         _player1 = new Player(Console.ReadLine(), 1);
 
@@ -200,10 +251,7 @@ class Connect4Game
         {
             _player2 = new AI("Computer", 2);
         }
-
-        _board = new Board();
         Display.PrintBoard(_board.BoardState, _win);
-        _turn = 0;
     }
 
     public static int GameMode()
@@ -215,6 +263,7 @@ class Connect4Game
             if (_mode != 1 && _mode != 2)
                 throw new ArgumentOutOfRangeException();
         }
+       
         catch
         {
             Console.WriteLine("Invalid game mode!");
@@ -237,42 +286,40 @@ class Connect4Game
     {
         _board.UpdateBoard(_player, _player.Turn().Result);
         _win = _board.CheckWin(_player.ID);
-        Display.PrintBoard(_board.BoardState, _win);
+        _boardFull = _board.IsBoardFull();
+        Display.PrintBoard(_board.BoardState, _win || _boardFull);
+
         if (_win)
         {
             if ((_mode != 1) && (_player.ID == 2))
-                Display.LosttoAI(_player.Name, _player.ID);     
+                Display.LosttoAI(_player.Name, _player.ID);
             else
                 Display.Winner(_player.Name, _player.ID);
         }
-        return (_win);
-    }
+        if (_boardFull && !_win)
+            Display.Draw();
 
+        return (_win || _boardFull);
+    }
 
     public static bool PlayAgain()
     {
-        Console.Write("Do you want to play again (Y/N)? ");
+        Console.Write("Do you want to play again (Y/N)? "); 
         string retry = Console.ReadLine().ToUpper();
         try
         {
             if (retry != "Y" && retry != "N")
                 throw new ArgumentException();
         }
+        
         catch
         {
             Console.WriteLine("Invalid option entered!");
             return PlayAgain();
         }
-        return retry == "Y";
+        return retry == "Y"; 
     }
 }
-
-
-
-
-
-
-
 
 
 public class Display
@@ -312,7 +359,7 @@ public class Display
             Console.WriteLine("-----------------------------------------");
         }
         else
-         {
+        {
             Console.WriteLine("-----------------------------------------");
             Console.WriteLine("| <<<<<<<<  VS. COMPUTER MODE  >>>>>>>> | ");
             Console.WriteLine("-----------------------------------------");
@@ -329,18 +376,20 @@ public class Display
     public static void PrintBoard(int[,] board, bool win)
     {
         Console.Clear();
-        DisplayTitle();
+        DisplayTitle(); 
+
         for (int i = 0; i < Board.Rows; i++)
         {
             Console.Write("|    ");
             for (int j = 0; j < Board.Columns; j++)
             {
-                Symbol(board[i, j]);
+                Symbol(board[i, j]); 
                 Console.Write("    ");
             }
             Console.Write("|");
             Console.WriteLine();
         }
+
         Console.WriteLine("-----------------------------------------");
         Console.WriteLine("|    1    2    3    4    5    6    7    |");
         Console.WriteLine("-----------------------------------------");
@@ -369,11 +418,16 @@ public class Display
     public static void LosttoAI(string name, int id)
     {
         Console.Write("| Sorry ");
-        Display.ColoredDisplay(name, id);
+        Display.ColoredDisplay(name, id); 
         Console.WriteLine(" wins! =( ");
         Console.WriteLine("=========================================");
     }
 
+    public static void Draw()
+    {
+        Console.WriteLine("| The game is a draw!                   |");
+        Console.WriteLine("=========================================");
+    }
 
     public static void Symbol(int id)
     {
@@ -382,7 +436,7 @@ public class Display
         else if (id == 2)
             ColoredDisplay("O", id);
         else
-            Console.Write("-");
+            Console.Write("-"); 
     }
 
     public static void ColoredDisplay(string str, int id)
@@ -396,17 +450,16 @@ public class Display
         Console.ResetColor();
     }
 }
-
-
 class Program
 {
     static void Main(string[] args)
     {
-        bool run = true;
+        bool run = true; 
+
         while (run)
         {
             Connect4Game.SetupGame();
-            while (Connect4Game.Play()) ;
+            while (Connect4Game.Play());
             run = Connect4Game.PlayAgain();
         }
     }
