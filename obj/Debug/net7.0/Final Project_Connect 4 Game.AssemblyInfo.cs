8dbe586f85329eff8b918aa84b1003d12e5d7b25
g.perez314 <g.perez314@mybvc.ca>
2024-04-19 00:50:05 -0600

    bug fixes

--- obj/Debug/net7.0/Final Project_Connect 4 Game.AssemblyInfo.cs
+++ obj/Debug/net7.0/Final Project_Connect 4 Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final Project_Connect 4 Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+561b91f0ca6bfba576e10dd831f035769bed6fe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final Project_Connect 4 Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final Project_Connect 4 Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
